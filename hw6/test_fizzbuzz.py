62fdbb320c75a6ccdf061b228735fff9e30ba48b
Garett Goodlake <39612949+Pyrothei@users.noreply.github.com>
2021-03-07 09:36:39 -0800

    delete hw6 directory to restart

--- hw6/test_fizzbuzz.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-   def test_fizz1(self):
-      self.assertEqual(fizzbuzz(3), "Fizz")
-
-if __name__ == '__main__':
-   unittest.main()

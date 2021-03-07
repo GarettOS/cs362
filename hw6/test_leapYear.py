67a2bde779a026aa4ebe9825b65ab206d3ac4466
Garett Goodlake <goodlakg@flip1.engr.oregonstate.edu>
2021-03-07 10:34:11 -0800

    added final test case to check a actual leap year

--- hw6/test_leapYear.py
+++ hw6/test_leapYear.py
@@ -8,6 +8,9 @@ class TestStringMethods(unittest.TestCase):
    def test_leap2(self):
       self.assertEqual(leapYear.leapYear(-1), "Not Leap Year")
 
+   def test_leap3(self):
+      self.assertEqual(leapYear.leapYear(2000), "Leap Year")
+
 if __name__ == '__main__':
    unittest.main()
 

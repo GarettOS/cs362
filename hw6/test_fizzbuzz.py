dc3c4f0db4b7eaf69eccfeb596743361515f2c93
Garett Goodlake <goodlakg@flip3.engr.oregonstate.edu>
2021-03-07 09:34:05 -0800

    wrote first test for fizzBuzz program

--- hw6/test_fizzbuzz.py
+++ hw6/test_fizzbuzz.py
@@ -4,20 +4,5 @@ class TestStringMethods(unittest.TestCase):
    def test_fizz1(self):
       self.assertEqual(fizzbuzz(3), "Fizz")
 
-   def test_fizz2(self):
-      self.assertEqual(fizzbuzz(21), "Fizz")
-
-   def test_fizz3(self):
-      self.assertEqual(fizzbuzz(5), "Buzz")
-
-   def test_fizz3(self):
-      self.assertEqual(fizzbuzz(10), "Buzz")
-
-   def test_fizz4(self):
-      self.assertEqual(fizzbuzz(30), "FizzBuzz")
-
-   def test_fizz5(self):
-      self.assertEqual(fizzbuzz(15), "FizzBuzz")
-
 if __name__ == '__main__':
    unittest.main()

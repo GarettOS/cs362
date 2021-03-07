be24d1894096f4e750543d49116c17a95e2a3553
Garett Goodlake <goodlakg@flip1.engr.oregonstate.edu>
2021-03-07 10:37:39 -0800

    implemented the rest where you tets if its a leap year

--- hw6/leapYear.py
+++ hw6/leapYear.py
@@ -2,4 +2,12 @@ def leapYear(x):
    if (x < 0):
       return "Not Leap Year"
    if (x % 4 == 0):
+      if (x % 100 == 0):
+         if (x % 400 == 0):
+            return "Leap Year"
+         else:
+            return "Not Leap Year"
+      else:
+         return "Not Leap Year"
+   else:
       return "Not Leap Year"

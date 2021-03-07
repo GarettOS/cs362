6a69086ff6a8b084b9d899ffd6423051671b1386
Garett Goodlake <goodlakg@flip1.engr.oregonstate.edu>
2021-03-07 10:31:57 -0800

    Added test case 2 for leapyear

--- hw6/test_leapYear.py
+++ hw6/test_leapYear.py
@@ -5,6 +5,9 @@ class TestStringMethods(unittest.TestCase):
    def test_leap1(self):
       self.assertEqual(leapYear.leapYear(4), "Not Leap Year")
 
+   def test_leap2(self):
+      self.assertEqual(leapYear.leapYear(-1), "Not Leap Year")
+
 if __name__ == '__main__':
    unittest.main()
 

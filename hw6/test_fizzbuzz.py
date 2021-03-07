bbc4d3f6ff62d0c927149d0e8d0b13c4e894b45f
Garett Goodlake <goodlakg@flip1.engr.oregonstate.edu>
2021-03-07 09:58:10 -0800

    Wrote 2nd test case for fizzbuzz

--- hw6/test_fizzbuzz.py
+++ hw6/test_fizzbuzz.py
@@ -5,5 +5,8 @@ class TestStringMethods(unittest.TestCase):
    def test_fizz1(self):
       self.assertEqual(fizzbuzz.fizzbuzz(3), 'Fizz')
 
+   def test_fizz2(self):
+      self.assertEqual(fizzbuzz.fizzbuzz(20), 'Buzz')
+
 if __name__ == '__main__':
    unittest.main()

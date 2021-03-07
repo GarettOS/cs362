1e559a21d21dedd6d4511e89937f093fac050876
Garett Goodlake <goodlakg@flip3.engr.oregonstate.edu>
2021-03-07 09:47:39 -0800

    Wrote the first test case for fizzbuzz

--- hw6/test_fizzbuzz.py
+++ hw6/test_fizzbuzz.py
@@ -1,8 +1,8 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+def TestStringMethods(unittest.TestCase):
    def test_fizz1(self):
-      self.assertEqual(fizzbuzz(3), "Fizz")
+      self.assertEqual(fizzbuzz(3), "fizz")
 
 if __name__ == '__main__':
    unittest.main()

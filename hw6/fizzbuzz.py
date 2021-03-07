f4447d8e975b4b53ac5f3983a734f80d6bd0e707
Garett Goodlake <goodlakg@flip1.engr.oregonstate.edu>
2021-03-07 10:03:57 -0800

    Wrote implementation for testing if a number if a multiple of both 3 and 5

--- hw6/fizzbuzz.py
+++ hw6/fizzbuzz.py
@@ -1,6 +1,9 @@
 def fizzbuzz(a):
-   if (a % 3 == 0):
-      return "Fizz"
+   if (a % 3 == 0 and a % 5 == 0):
+      return "FizzBuzz"
 
    elif(a % 5 == 0):
       return "Buzz"
+
+   elif(a % 3 == 0):
+      return "Fizz"
